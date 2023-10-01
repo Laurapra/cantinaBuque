cba87c811cc2a2a4d7e1928cfc158507acc57440
laura <laurapra@MacBook-Air-de-Diana.local>
2023-09-30 22:24:38 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { PrimeReactProvider } from 'primereact/api';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <PrimeReactProvider>
+        <h1 className="text-red-500 bg-blue-500">SOsY PRO</h1>
+      </PrimeReactProvider>
+    </>
+  )
+}
+
+export default App
